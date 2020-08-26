f764e6ff5772a55cb465e42f2e39255b765e441a
Ridwan Arifin <ridwanarifin@localhost.localdomain>
2020-08-26 08:35:04 -0400

    refactor revisi

--- components/navigations/index.vue
+++ components/navigations/index.vue
@@ -314,7 +314,10 @@ $secondary: #232323;
       box-shadow .8s ease-in-out;
     &.animated {
       // transform: translateY(-60%); //1280
-      transform: translateY(-65%); //1440
+      transform: translateY(-50%); //1204
+      @media (min-width: 1440px) {
+        transform: translateY(-58%) !important;
+      }
       box-shadow:
         0 3px 5px -1px rgba(0,0,0,.2),
         0 6px 10px 0 rgba(0,0,0,.14),
