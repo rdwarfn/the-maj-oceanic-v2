b4552f6fcd06230087e685b54a0fed680c2e5d54
Ridwan Arifin <ridwanarifin@localhost.localdomain>
2020-10-25 13:53:17 +0700

    test netlify v1

---
+++ pages/develop.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container class="mx-auto px-6 px-md-0">
+    <nuxt-content :document="data" />
+  </v-container>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const data = await $content('articles').fetch()
+
+    return {
+      data
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
