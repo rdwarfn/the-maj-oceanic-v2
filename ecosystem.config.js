ade95041a95e15caad82590dec30e8819d351139
davidasrobot <yogasakajr@gmail.com>
2021-09-02 00:09:53 +0000

    reconfig deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,18 @@
 module.exports = {
   apps: [
     {
-      name: 'Oceanic',
-      exec_mode: 'cluster',
-      instances: 1, // Or a number of instances
+      name: 'TMO',
+      instances: 1,
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
       env: {
-        PORT: 3001,
-        NODE_ENV: 'production'
+        PORT: 3002,
+        NODE_ENV: 'production',
+        NODE_TLS_REJECT_UNAUTHORIZED: 0,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        NODE_TLS_REJECT_UNAUTHORIZED: 0,
       }
     }
   ]
